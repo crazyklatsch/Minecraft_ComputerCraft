8dd92e3d6218a3012ea316dc9ae4f2cd0df53aac
Max Körsten <maxkoersten@gmx.net>
2022-05-06 19:20:28 +0200

    test?

--- turtle/startup.lua
+++ turtle/startup.lua
@@ -11,7 +11,7 @@ if not fs.exists('/master_pc_id') then
 end
 
 
-multishell.launch(os.getenv(), '/turtle_statemachine.lua')
-multishell.launch(os.getenv(), '/report_state.lua')
+multishell.launch(getenv(), '/turtle_statemachine.lua')
+multishell.launch(getenv(), '/report_state.lua')
 multishell.setTitle(2, 'statemachine')
 multishell.setTitle(3, 'report_state')

f1d3247169d1d0120e158616e33d586fe7e3ad3a
Max Körsten <maxkoersten@gmx.net>
2022-05-06 16:51:47 +0200

    added hub setup

---
+++ hub_setup.lua
@@ -0,0 +1,15 @@
+shell.run('rm', '/git.lua')
+shell.run('rm', '/common')
+shell.run('wget', 'https://raw.github.com/crazyklatsch/Minecraft_ComputerCraft/main/git.lua')
+require('git')
+git.get('common')
+git.get('hub')
+git.get('hub_setup.lua')
+local mylist = fs.list('hub/')
+for i=1, #mylist do
+    shell.run('rm', mylist[i])
+end
+shell.run('mv', '/hub/*', '/')
+fs.delete('hub')
+---@diagnostic disable-next-line: undefined-field
+os.reboot()

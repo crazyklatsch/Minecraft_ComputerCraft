ab78af00f7ffb2f8037de8a849e3741d68b4db2f
Sandruin <sebastian.lederer@yandex.com>
2022-05-07 21:01:51 +0200

    fe

--- common/logging.lua
+++ common/logging.lua
@@ -7,12 +7,19 @@ function log(msg, log_level, terminal, pcid)
     local log_level = log_level or log_levels.INFO
     local terminal = terminal or term
 
-    local message = '[' .. log_level_color[log_level] .. log_level_strings[log_level] .. '&0]'
+    local message = ""
     -- append pcid if available
     if pcid then
-        message = '[' .. pcid .. ']' .. message
+        message = '[' .. pcid .. ']'
+    end
+    if log_level == log_levels.DEBUG then
+        -- make entire line light gray to be less intrusive
+        message = log_level_color[log_level] .. message .. '[' .. log_level_strings[log_level] .. ']'
+    else
+        message = message .. '[' .. log_level_color[log_level] .. log_level_strings[log_level] .. '&0]'
     end
     message = message .. ' ' .. msg
+
     -- send message to list of logging_ids
     local logging_ids = settings.get(config.logging_ids)
     if logging_ids then
--- hub/startup.lua
+++ hub/startup.lua
@@ -1,3 +1,7 @@
+local completion = require "cc.shell.completion"
+require("common.turtle_interface")
+
+
 -- set label
 ---@diagnostic disable-next-line: undefined-field
 os.setComputerLabel('Master PC ' .. os.getComputerID())
@@ -5,7 +9,7 @@ os.setComputerLabel('Master PC ' .. os.getComputerID())
 -- open rednet
 -- cannot differentiate between ender modems and wired modems
 -- but opening rednet isnt bad right??
-for _, side in pairs({'back', 'top', 'left', 'right'}) do
+for _, side in pairs({ 'back', 'top', 'left', 'right' }) do
     if peripheral.getType(side) == 'modem' then
         rednet.open(side)
         break
@@ -16,3 +20,9 @@ multishell.launch(_ENV, '/event_listener.lua')
 multishell.launch(_ENV, '/monitor.lua')
 multishell.setTitle(2, 'event listener')
 multishell.setTitle(3, 'monitor')
+
+
+local complete_turtle_control = completion.build(
+    { completion.choice, command }, { completion.choice, action }
+)
+shell.setCompletionFunction("control_turtle.lua", complete_turtle_control)

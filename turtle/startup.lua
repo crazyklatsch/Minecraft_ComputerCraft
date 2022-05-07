f35541577c73f43559ae8c761fd066979afc5e2f
Max Körsten <maxkoersten@gmx.net>
2022-05-08 01:07:49 +0200

    lele

--- turtle/startup.lua
+++ turtle/startup.lua
@@ -1,7 +1,6 @@
 require('common.globals')
 require('common.utils')
 require('common.logging')
-require('common.watchdog')
 
 -- set label
 ---@diagnostic disable-next-line: undefined-field
@@ -15,8 +14,8 @@ settings.define(config.turtle_move_wait, { default = 1.0, type = "number" })
 settings.define(config.logging_ids, { default = { settings.get(config.master_pc_id) }, type = "table" })
 
 
---local id_statemachine = multishell.launch(_ENV, '/common/watchdog.lua', '/turtle_statemachine.lua')
-local id_statemachine = multishell.launch(_ENV, '/turtle_statemachine.lua')
+local id_statemachine = multishell.launch(_ENV, '/common/watchdog.lua', '/turtle_statemachine.lua')
+--local id_statemachine = multishell.launch(_ENV, '/turtle_statemachine.lua')
 local id_reportstate = multishell.launch(_ENV, '/report_state.lua')
 multishell.setTitle(id_statemachine, 'state manager')
 multishell.setTitle(id_reportstate, 'status report')

97734823ae6036c2b884e845078247ca3fef4491
Max Körsten <maxkoersten@gmx.net>
2022-05-08 01:01:04 +0200

    :(

--- turtle/startup.lua
+++ turtle/startup.lua
@@ -15,7 +15,8 @@ settings.define(config.turtle_move_wait, { default = 1.0, type = "number" })
 settings.define(config.logging_ids, { default = { settings.get(config.master_pc_id) }, type = "table" })
 
 
-local id_statemachine = multishell.launch(_ENV, '/common/watchdog.lua', '/turtle_statemachine.lua')
+--local id_statemachine = multishell.launch(_ENV, '/common/watchdog.lua', '/turtle_statemachine.lua')
+local id_statemachine = multishell.launch(_ENV, '/turtle_statemachine.lua')
 local id_reportstate = multishell.launch(_ENV, '/report_state.lua')
 multishell.setTitle(id_statemachine, 'state manager')
 multishell.setTitle(id_reportstate, 'status report')
